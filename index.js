9e96bfe2f04bf759b1f6c90ae07f72c10b4de48e
jaishriram86 <mohans86700@gmail.com>
2025-07-21 21:34:10 +0530

    added a test api

--- index.js
+++ index.js
@@ -26,6 +26,14 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
+// for test api
+app.get("/", (req, res) => {
+  return res.status(200).json({
+    success: true,
+    msg: "Successfully running",
+  });
+});
+
 // for redirecting the urls with original urls
 app.get("/url/:shortId", async (req, res) => {
   const shortId = req.params.shortId;
@@ -46,7 +54,7 @@ app.get("/url/:shortId", async (req, res) => {
 
 app.use("/user", userRoute); // for signup and login
 app.use("/url", restrictLoggedUserOnly, urlRoute); // for auth verify and then allowing to generate short url
-app.use("/", checkAuth, staticRoute); // for getting the logged in user's url
+app.use("/get-urls", checkAuth, staticRoute); // for getting the logged in user's url
 
 const PORT = process.env.PORT || 8001;
 app.listen(PORT, () => console.log(`Server is running at PORT:${PORT}`));
